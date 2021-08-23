babefd472b528e1e73682750fafd702b13c96017
dhershman1 <dustinh17@gmail.com>
2021-08-23 14:48:08 -0400

    Added: Bin file

---
+++ bin/tapidatcher.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import minimist from 'minimist'
+import tapidatcher from '../index.js'
+
+/* Modules */
+const parsedArgs = minimist(process.argv.slice(2), {
+  alias: {
+    v: 'version',
+    t: 'tests',
+    s: 'src',
+    f: 'initial',
+    c: 'cmd',
+    i: 'inline',
+    n: 'env',
+    p: 'pipe',
+    e: 'ending',
+    x: 'ignore'
+  },
+  default: {
+    e: '.spec.js',
+    c: 'tape'
+  }
+})
+
+/* Output */
+if (parsedArgs.version) {
+  // Placeholder until a better json handling by ES6 imports
+  // https://github.com/tc39/proposal-json-modules
+  console.log('v0.1.0')
+} else {
+  tapidatcher(parsedArgs)
+}
